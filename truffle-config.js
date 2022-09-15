017c15974e4afaf0bd5ec7c3beb71db86310b746
Dan Magro <djm385@icloud.com>
2022-09-15 15:07:24 -0400

    completed project

--- truffle-config.js
+++ truffle-config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 3000,
       network_id: "*" // Match any network id
     },
     kovan: {
